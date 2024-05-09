c66bb77ff092236a574968f1ec198759bd3d7dbc
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-09 13:11:31 +0300

    Method naming convention applied.

--- XUnitTest/XUnitTest.App/Calculator.cs
+++ XUnitTest/XUnitTest.App/Calculator.cs
@@ -4,6 +4,7 @@
 	{
 		public int Add(int a,int b)
 		{
+			if(a==b) return 0;
 			return a + b;
 		}
 	}
--- XUnitTest/XUnitTest.Test/CalculatorTest.cs
+++ XUnitTest/XUnitTest.Test/CalculatorTest.cs
@@ -5,17 +5,49 @@ namespace XUnitTest.Test
 {
 	public class CalculatorTest
 	{
-		[Fact]
-		public void AddTest()
+
+		public Calculator Calculator { get; set; }
+        public CalculatorTest()
+        {
+            this.Calculator = new Calculator();
+        }
+
+
+        //parametre almayan test metotlarında[Fact] attribute u kullanılır.
+
+        [Fact]
+		public void AddTest_ReturnTotalValue_WhenAandBIsDifferentValue()
 		{
 			//Arrange
 			int a = 10;
 			int b = 20;
-			Calculator calculator = new();
 			//Act
-			var total= calculator.Add(a, b);
+			var total= Calculator.Add(a, b);
 			//Assert
 			Assert.Equal<int>(30, total);
+		}
+
+		[Fact]
+		public void AddTest_ReturnZero_WhenAandBIsSameValue()
+		{
+			//Arrange
+			int a = 10;
+			int b = 10;
+			//Act
+			var total = Calculator.Add(a, b);
+			//Assert
+			Assert.Equal<int>(0, total);
+		}
+		//parametre alan test metotlarında [Theory] ve [InlineData] metotları birlikte kullanılır.
+
+		[Theory]
+		[InlineData(2,5,7)]
+		[InlineData(10,2,12)]
+
+		public void TestWithParameters(int a, int b, int expectedTotal)
+		{
+			var actualTotal= Calculator.Add(a, b);
+			Assert.Equal(expectedTotal, actualTotal);
 		}
 	}
 }
