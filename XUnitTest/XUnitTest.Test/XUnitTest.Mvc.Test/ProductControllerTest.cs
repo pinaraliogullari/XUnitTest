b6dbf90e1a0704001b5930a76edb5df2fba5c64f
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-10 16:12:46 +0300

    Update method tested.

--- XUnitTest/XUnitTest.Test/XUnitTest.Mvc.Test/ProductControllerTest.cs
+++ XUnitTest/XUnitTest.Test/XUnitTest.Mvc.Test/ProductControllerTest.cs
@@ -126,12 +126,112 @@ namespace XUnitTest.Test.XUnitTest.Mvc.Test
 
 		public async Task Create_CreateMethodNotExecute_WhenModelStateIsNotValid()
 		{
+			var productId = Guid.NewGuid();
+
 			var mock=new Mock<IRepository<Product>>();
 			var controller= new ProductController(mock.Object);
 			controller.ModelState.AddModelError("name", "Name alanı boş bırakılamaz");
 
-			var result=await controller.Create(It.IsAny<Product>());
+			var result=await controller.Create(new Product());
 			mock.Verify(x => x.CreateAsync(It.IsAny<Product>()), Times.Never());
 		}
+
+		[Fact]
+		public async Task Edit_ReturnRedirectoIndexAction_WhenIdIsNull()
+		{
+			var mock = new Mock<IRepository<Product>>();
+			var controller = new ProductController(mock.Object);
+
+			var result=await controller.Edit(null);
+			var redirect=Assert.IsType<RedirectToActionResult>(result);
+			Assert.Equal("Index", redirect.ActionName);
+		}
+
+		[Fact]
+		public async Task Edit_ReturnNotFound_WhenIdIsNotValid()
+		{
+			Product product = null;
+			var productId = Guid.NewGuid();
+
+			var mock= new Mock<IRepository<Product>>();
+			mock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(product);
+			var controller=new ProductController(mock.Object);
+
+			var result = await controller.Edit(productId);
+			var redirect = Assert.IsType<NotFoundResult>(result);
+			Assert.Equal(404,redirect.StatusCode);
+		}
+
+		[Fact]
+		public async Task Edit_ReturnProduct_WhenIdIsValid()
+		{
+			var productId= Guid.NewGuid();
+			var mock= new Mock<IRepository<Product>>();
+			mock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(new Product() { Id= productId });
+			var controller=new ProductController(mock.Object);
+
+			var result= await controller.Edit(productId);
+			var viewResult = Assert.IsType<ViewResult>(result);
+		    var resultProduct=Assert.IsAssignableFrom<Product>(viewResult.Model);
+			Assert.Equal<Guid>(productId, resultProduct.Id);
+		}
+
+		[Fact]
+		public void Edit_ReturnNotFound_WhenIdIsNotEqualProductId()
+		{
+			var guidId = Guid.NewGuid();
+			var productId = new Product() { Id = Guid.NewGuid() };
+
+			var mock =new Mock<IRepository<Product>>();
+			var controller=new ProductController(mock.Object);
+
+			var result = controller.Edit(guidId, productId);
+			var redirect=Assert.IsType<NotFoundResult>(result);
+
+		}
+
+		[Fact]
+		public void Edit_ReturnView_WhenModelStateIsNotValid()
+		{
+			var guidId = Guid.NewGuid();
+			var productId = new Product() { Id = guidId };
+
+			var mock=new Mock<IRepository<Product>>();
+			var controller= new ProductController(mock.Object);
+			controller.ModelState.AddModelError("Name", "Name alanı boş bırakılamaz");
+
+			var result= controller.Edit(guidId,productId);
+			var viewResult = Assert.IsType<ViewResult>(result);
+			Assert.IsType<Product>(viewResult.Model);
+		}
+
+		[Fact]
+		public void Edit_ReturnRedirectToIndexAction_WhenModelStateIsValid()
+		{
+			var guidId = Guid.NewGuid();
+			var productId = new Product() { Id = guidId };
+
+			var mock= new Mock<IRepository<Product>>();
+			var controller = new ProductController(mock.Object);
+
+			var result=controller.Edit(guidId,productId);
+			var redirect= Assert.IsType<RedirectToActionResult>(result);
+			Assert.Equal("Index", redirect.ActionName);
+		}
+
+		[Fact]
+		public void Edit_UpdateMethodExecute_WhenModelStateIsValid()
+		{
+			var guidId = Guid.NewGuid();
+			var productId = new Product() { Id = guidId };
+
+			var mock = new Mock<IRepository<Product>>();
+			mock.Setup(x => x.UpdateAsync(It.IsAny<Product>()));
+			var controller=new ProductController(mock.Object);
+
+			var result= controller.Edit(guidId,productId);
+			mock.Verify(x=>x.UpdateAsync(It.IsAny<Product>()),Times.Once());
+		}
+	
 	}
 }
